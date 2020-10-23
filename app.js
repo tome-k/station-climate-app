5140203137aa0e8fad6bb4bd9e7d023ac1b7a52b
nicholas-bd <nicholas.bond.d@gmail.com>
2020-10-23 16:34:15 +0200

    Add slideshow JS

--- app.js
+++ app.js
@@ -55,3 +55,23 @@ function dateBuilder(d) {
     return `${day} ${date} ${month} ${year}${sep} ${time}`;
 
 }
+
+const slideshowImages = document.querySelectorAll(".slideshow img");
+
+const nextImageDelay = 6000;
+let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+
+// slideshowImages[currentImageCounter].style.display = "block";
+slideshowImages[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+    // slideshowImages[currentImageCounter].style.display = "none";
+    slideshowImages[currentImageCounter].style.opacity = 0;
+
+    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+
+    // slideshowImages[currentImageCounter].style.display = "block";
+    slideshowImages[currentImageCounter].style.opacity = 1;
+}
